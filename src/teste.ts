d2be8cea994e17c9f39e997713ef31fb3aa8a47b
ESKieroff <rodrigo.rosa81@edu.pucrs.br>
2024-08-26 14:01:27 -0300

    teste husky

--- src/teste.ts
+++ src/teste.ts
@@ -2,3 +2,12 @@
 
 const mensagem = 'Olá, mundo!'
 console.log(mensagem)
+const soma = (a: number, b: number): number => {
+  return a + b
+}
+console.log(soma(2, 3))
+
+const funcao1 = (): string => {
+  return 'Teste'
+}
+console.log(funcao1())
