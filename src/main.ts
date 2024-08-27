11b1dc4491d1f45106c9fbdcad6a2d51644f4566
ESKieroff <rodrigo.rosa81@edu.pucrs.br>
2024-08-27 12:45:07 -0300

    feat: changes included

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import 'reflect-metadata'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap (): Promise<void> {
+  const app = await NestFactory.create(AppModule)
 
-
-
-
-
-  
   const config = new DocumentBuilder()
-              .setTitle('API Documentation')
+    .setTitle('API Documentation')
     .setDescription('The API description')
     .setVersion('1.0')
     .addTag('users')
-    .build();
+    .build()
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
 
-  await app.listen(3000);
+  await app.listen(3000)
 }
-bootstrap();
-
-console.log('This should cause an ESLint error');
+await bootstrap()
