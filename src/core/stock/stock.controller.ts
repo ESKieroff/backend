284c25ecbe045150a0009a7d10c6e50fb14297ff
Vicenzo Frusciante <v.frusciante@edu.pucrs.br>
2024-09-26 18:31:48 -0300

    feat(stock): add endpoints for stock management

--- src/core/stock/stock.controller.ts
+++ src/core/stock/stock.controller.ts
@@ -11,7 +11,7 @@ import { StockService } from './stock.service';
 import { CreateStockDto } from './dto/create-stock.dto';
 import { UpdateStockDto } from './dto/update-stock.dto';
 
-@Controller('stok')
+@Controller('stock')
 export class StockController {
   constructor(private readonly stockService: StockService) {}
 
@@ -38,5 +38,18 @@ export class StockController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.stockService.remove(+id);
+  }
+
+  @Post('add-raw-material/:id')
+  addRawMaterial(@Param('id') id: string, @Body('quantity') quantity: number) {
+    return this.stockService.addRawMaterial(+id, quantity);
+  }
+
+  @Post('remove-raw-material/:id')
+  removeRawMaterial(
+    @Param('id') id: string,
+    @Body('quantity') quantity: number
+  ) {
+    return this.stockService.removeRawMaterial(+id, quantity);
   }
 }
--- src/core/stock/stock.service.ts
+++ src/core/stock/stock.service.ts
@@ -5,22 +5,30 @@ import { UpdateStockDto } from './dto/update-stock.dto';
 @Injectable()
 export class StockService {
   create(_createStockDto: CreateStockDto) {
-    return 'This action adds a new stok';
+    return 'This action adds a new stock';
   }
 
   findAll() {
-    return 'This action returns all stok';
+    return 'This action returns all stock';
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} stok`;
+    return `This action returns a #${id} stock`;
   }
 
   update(id: number, _updateStockDto: UpdateStockDto) {
-    return `This action updates a #${id} stok`;
+    return `This action updates a #${id} stock`;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} stok`;
+    return `This action removes a #${id} stock`;
+  }
+
+  addRawMaterial(id: number, quantity: number) {
+    return `This action adds ${quantity} of raw material to stock #${id}`;
+  }
+
+  removeRawMaterial(id: number, quantity: number) {
+    return `This action removes ${quantity} of raw material from stock #${id}`;
   }
 }
