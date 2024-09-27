894df7c72fbd95748da20fd052f5abdc7d667b47
ESKieroff <rodrigo.rosa81@edu.pucrs.br>
2024-09-27 12:18:32 -0300

    feat: dto for update stock created

--- src/core/stock/dto/update-stock.dto.ts
+++ src/core/stock/dto/update-stock.dto.ts
@@ -3,12 +3,63 @@ import { CreateStockDto } from './create-stock.dto';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateStockDto extends PartialType(CreateStockDto) {
-  @ApiProperty()
-  readonly name: string;
+  @ApiProperty({
+    description: 'Quantity',
+    example: '1'
+  })
+  readonly quantity: number;
 
-  @ApiProperty()
-  readonly price: number;
+  @ApiProperty({
+    description: 'Unit price',
+    example: '1.00',
+    required: false
+  })
+  readonly unit_price: number;
 
-  @ApiProperty()
-  readonly stock: number;
+  @ApiProperty({
+    description: 'Total price',
+    example: '1.00',
+    required: false
+  })
+  readonly total_price: number;
+
+  @ApiProperty({
+    description: 'Lote',
+    example: '1'
+  })
+  readonly lote: string;
+
+  @ApiProperty({
+    description: 'Expiration',
+    example: '2021-05-01'
+  })
+  readonly expiration: Date;
+
+  @ApiProperty({
+    description: 'Supplier',
+    example: '1',
+    required: false
+  })
+  readonly supplier: number;
+
+  @ApiProperty({
+    description: 'Costumer',
+    example: '1',
+    required: false
+  })
+  readonly costumer: number;
+
+  @ApiProperty({
+    description: 'Stock location id',
+    example: '1',
+    required: false
+  })
+  readonly stock_location_id: number;
+
+  @ApiProperty({
+    description: 'Observation',
+    example: '1',
+    required: false
+  })
+  readonly observation: string;
 }
