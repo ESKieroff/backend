c17f0a0d73fac7c5987db37ad5820e2564d6aa48
ESKieroff <rodrigo.rosa81@edu.pucrs.br>
2024-09-27 12:15:04 -0300

    feat: created dto for create stock

--- src/core/stock/dto/create-stock.dto.ts
+++ src/core/stock/dto/create-stock.dto.ts
@@ -1,12 +1,93 @@
-import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateStockDto {
-  @IsNotEmpty()
-  readonly name: string;
+  @ApiProperty({
+    description: 'Document date',
+    example: '2021-05-01'
+  })
+  readonly document_date: Date;
 
-  @IsNotEmpty()
-  readonly price: number;
+  @ApiProperty({
+    description: 'Stock moviment',
+    example: ['INPUT', 'OUTPUT', 'TRANSIT']
+  })
+  readonly stock_moviment: string;
 
-  @IsNotEmpty()
-  readonly stock: number;
+  @ApiProperty({
+    description: 'Stock id',
+    example: '1'
+  })
+  readonly stock_id: number;
+
+  @ApiProperty({
+    description: 'Sequence',
+    example: '1'
+  })
+  readonly sequence: number;
+
+  @ApiProperty({
+    description: 'Product id',
+    example: '1'
+  })
+  readonly product_id: number;
+
+  @ApiProperty({
+    description: 'Quantity',
+    example: '1'
+  })
+  readonly quantity: number;
+
+  @ApiProperty({
+    description: 'Unit price',
+    example: '1.00',
+    required: false
+  })
+  readonly unit_price: number;
+
+  @ApiProperty({
+    description: 'Total price',
+    example: '1.00',
+    required: false
+  })
+  readonly total_price: number;
+
+  @ApiProperty({
+    description: 'Lote',
+    example: '1'
+  })
+  readonly lote: string;
+
+  @ApiProperty({
+    description: 'Expiration',
+    example: '2021-05-01'
+  })
+  readonly expiration: Date;
+
+  @ApiProperty({
+    description: 'Supplier',
+    example: '1',
+    required: false
+  })
+  readonly supplier: number;
+
+  @ApiProperty({
+    description: 'Costumer',
+    example: '1',
+    required: false
+  })
+  readonly costumer: number;
+
+  @ApiProperty({
+    description: 'Stock location id',
+    example: '1',
+    required: false
+  })
+  readonly stock_location_id: number;
+
+  @ApiProperty({
+    description: 'Observation',
+    example: '1',
+    required: false
+  })
+  readonly observation: string;
 }
