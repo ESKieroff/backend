b459611fe2f386aeb86a15df95f797a7042f2e4b
ESKieroff <rodrigo.rosa81@edu.pucrs.br>
2024-10-03 21:43:01 -0300

    feat: module updated

--- src/core/stock/stock.module.ts
+++ src/core/stock/stock.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StockService } from './stock.service';
 import { StockController } from './stock.controller';
+import { StockRepository } from './stock.repository';
+import { PrismaModule } from 'src/database/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [StockController],
-  providers: [StockService]
+  providers: [StockService, StockRepository]
 })
 export class StockModule {}
