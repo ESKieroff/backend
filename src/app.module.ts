7549586dc3a191954c306d63981caa53f597c145
ESKieroff <rodrigo.rosa81@edu.pucrs.br>
2024-10-05 11:47:07 -0300

    feat: full module persons

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,8 +11,8 @@ import { Config } from './config/config.module';
 import { PrismaModule } from './database/prisma/prisma.module';
 import { PrismaService } from './database/prisma/prisma.service';
 import { FeatureFlagsService } from './feature-flags/feature-flags.service';
+import { PersonsModule } from './core/persons/persons.module';
 // import { CategoriesModule } from './core/categories/categories.module';
-// import { PersonsModule } from './core/persons/persons.module';
 // import { GroupsModule } from './core/groups/groups.module';
 // import { CompositionsModule } from './core/compositions/compositions.module';
 
@@ -26,9 +26,9 @@ import { FeatureFlagsService } from './feature-flags/feature-flags.service';
     AuthModule,
     ControlPanelModule,
     Config,
-    PrismaModule
+    PrismaModule,
+    PersonsModule
     // CategoriesModule,
-    // PersonsModule,
     // GroupsModule,
     // CompositionsModule
   ],
