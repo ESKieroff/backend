f9fdf7962f2717e8d98de841be45c1827bb80a2f
ESKieroff <rodrigo.rosa81@edu.pucrs.br>
2024-10-10 01:38:52 -0300

    fix: linter on files

--- src/core/users/dto/user.schema.ts
+++ src/core/users/dto/user.schema.ts
@@ -3,7 +3,7 @@ import * as z from 'zod';
 export const CreateUserSchema = z.object({
   email: z.string().email('Email is required'),
   password: z.string().min(6, 'Password is required'),
-  role: z.string().default('DEFAULT').optional(),
+  role: z.string().default('DEFAULT').optional()
 });
 
 export const UpdateUserSchema = z.object({
