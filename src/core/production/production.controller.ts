7ba54725b273564d3ecd9723f3560209a28b7c93
VitoriaFrohlich <vitoriafrohlichh@gmail.com>
2024-10-14 02:04:35 -0300

    feat: update production controller

--- src/core/production/production.controller.ts
+++ src/core/production/production.controller.ts
@@ -5,11 +5,18 @@ import {
   Body,
   Patch,
   Param,
-  Delete
+  Delete,
+  Query,
+  //NotFoundException,
+  BadRequestException
 } from '@nestjs/common';
 import { ProductionService } from './production.service';
 import { CreateProductionDto } from './dto/create-production.dto';
 import { UpdateProductionDto } from './dto/update-production.dto';
+import { ResponseProductionDto } from './dto/response.production.dto';
+import { ApiQuery } from '@nestjs/swagger';
+// import { ZodError } from 'zod';
+// import { Production } from './entities/production.entity';
 
 @Controller('production')
 export class ProductionController {
@@ -21,8 +28,48 @@ export class ProductionController {
   }
 
   @Get()
-  findAll() {
-    return this.productionService.findAll();
+  @ApiQuery({
+    name: 'orderBy',
+    required: false,
+    description:
+      'Field to order by. Valid fields: id, description, code, sku, category_id, group_id, supplier_id',
+    enum: [
+      'id',
+      'number',
+      'description',
+      'production_date',
+      'Production_Status',
+      'created_by',
+      'updated_by'
+    ]
+  })
+  async findAll(
+    @Query('orderBy') orderBy: string = 'id'
+  ): Promise<ResponseProductionDto[]> {
+    const validOrderFields = [
+      'id',
+      'number',
+      'description',
+      'production_date',
+      'Production_Status',
+      'created_by',
+      'updated_by'
+    ];
+
+    if (!validOrderFields.includes(orderBy)) {
+      throw new BadRequestException(`Invalid order field: ${orderBy}`);
+    }
+
+    const production = await this.productionService.findAll(orderBy);
+    return production.map(production => ({
+      id: production.id,
+      number: production.number,
+      description: production.description,
+      production_date: production.production_date,
+      created_by: production.created_by,
+      updated_by: production.updated_by,
+      Production_Status: production.Production_Status
+    }));
   }
 
   @Get(':id')
