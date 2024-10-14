9e87d06862bc005ba50f4913e11f2ca7e7881558
VitoriaFrohlich <vitoriafrohlichh@gmail.com>
2024-10-14 02:27:51 -0300

    feat: implements production repository and service

--- src/core/production/production.repository.ts
+++ src/core/production/production.repository.ts
@@ -1,129 +1,88 @@
-// import { Injectable } from '@nestjs/common';
-// import { PrismaService } from 'src/database/prisma/prisma.service';
-// import { Production, Prisma } from '@prisma/client';
-// import { Order } from './entities/order.entity';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { production_orders, Prisma } from '@prisma/client';
 
-// @Injectable()
-// export class OrderRepository {
-//   constructor(private prisma: PrismaService) {}
+@Injectable()
+export class ProductionRepository {
+  constructor(private prisma: PrismaService) {}
 
-//   async create(data: Prisma.ProductionCreateInput): Promise<Production> {
-//   const order = await this.prisma.production.create({
-//         data
-//       });
-//     const orderResponse = {
-//            ...order
-//           };
-//            return orderResponse;
-//          }
-// //Precisa da order no bd
-//   async findAll(orderBy: string): Promise<Order[]> {
-//   const validOrderFields = [
-//     'id',
-//     'description',
-//     'number',
-//     'product_quantity_estimated',
-//     'product_quantity_real',
-//     'lote'
-//   ];
+  async create(
+    data: Prisma.production_ordersCreateInput
+  ): Promise<production_orders> {
+    const order = await this.prisma.production_orders.create({
+      data
+    });
 
-//   if (!validOrderFields.includes(orderBy)) {
-//     throw new Error('Invalid order field');
-//   }
+    const productionResponse = {
+      ...order
+    };
+    return productionResponse;
+  }
+  //Precisa da order no bd
+  async findAll(orderBy: string): Promise<production_orders[]> {
+    const order = [
+      'id',
+      'number',
+      'description',
+      'production_date',
+      'Production_Status',
+      'created_by',
+      'updated_by'
+    ];
+    if (!order.includes(orderBy)) {
+      throw new Error('Invalid order field');
+    }
 
-//   const result = await this.prisma.production.findMany({
-//     where: { active: true },
-//     orderBy: { [orderBy]: 'asc' }
-//   });
+    const result = await this.prisma.production_orders.findMany({
+      orderBy: { [orderBy]: 'asc' }
+    });
+    return result;
+  }
 
-//   return result;
-// }
-// async findById(id: number): Promise< Order| null> {
-//     const orders = this.prisma.production.findUnique({
-//       where: { id }
-//     });
+  async findById(id: number): Promise<production_orders | null> {
+    const order = this.prisma.production_orders.findUnique({
+      where: { id }
+    });
 
-//     const productWithoutSensitiveFields = {
-//           ...orders,
-//           active: undefined
-//         };
+    if (!order) {
+      return null;
+    }
 
-//         return productWithoutSensitiveFields;
-//       }
-//     }
+    return order;
+  }
+  async matchProductionByData(
+    number: number
+    //   name:string,
+    //   price:number,
+    //   stock:number,
+    //   items
+  ): Promise<production_orders[]> {
+    return this.prisma.production_orders.findMany({
+      where: {
+        OR: [{ id: { equals: number } }]
+      }
+    });
+  }
+  async update(
+    id: number,
+    data: Prisma.production_ordersUpdateInput
+  ): Promise<production_orders> {
+    const product = this.prisma.production_orders.update({
+      where: { id },
+      data
+    });
 
-// // async findById(id: number): Promise<products | null> {
-// //   const product = this.prisma.products.findUnique({
-// //     where: { id }
-// //   });
-
-// //   const productWithoutSensitiveFields = {
-// //     ...product,
-// //     active: undefined
-// //   };
-
-// //   return productWithoutSensitiveFields;
-// // }
-
-// // async findManyByIds(ids: number[]): Promise<products[]> {
-// //   return this.prisma.products.findMany({
-// //     where: {
-// //       id: { in: ids }
-// //     }
-// //   });
-// // }
-
-// // async bulkUpdateStatus(ids: number[], data: Partial<products>) {
-// //   await this.prisma.products.updateMany({
-// //     where: {
-// //       id: { in: ids }
-// //     },
-// //     data
-// //   });
-// // }
-
-// // async matchProductByData(
-// //   code: string,
-// //   description: string,
-// //   sku: string
-// // ): Promise<products[]> {
-// //   return this.prisma.products.findMany({
-// //     where: {
-// //       OR: [
-// //         { code: { equals: code } },
-// //         { description: { equals: description } },
-// //         { sku: { equals: sku } }
-// //       ]
-// //     }
-// //   });
-// // }
-
-// // async update(
-// //   id: number,
-// //   data: Prisma.productsUpdateInput
-// // ): Promise<products> {
-// //   const product = this.prisma.products.update({ where: { id }, data });
-
-// //   const productResponse = {
-// //     ...product
-// //   };
-// //   return productResponse;
-// // }
-
-// // async delete(id: number): Promise<void> {
-// //   if (!id) {
-// //     throw new Error('ID not found');
-// //   }
-// //   const existingProduct = await this.findById(id);
-// //   if (!existingProduct) {
-// //     throw new Error('Product not found');
-// //   }
-
-// //   await this.prisma.products.update({
-// //     where: { id },
-// //     data: {
-// //       active: false,
-// //       updated_at: new Date()
-// //     }
-// //   });
-// // }
+    const productionResponse = {
+      ...product
+    };
+    return productionResponse;
+  }
+  async delete(id: number): Promise<void> {
+    await this.prisma.production_orders.update({
+      where: { id },
+      data: {
+        updated_at: new Date()
+      }
+    });
+  }
+}
--- src/core/production/production.service.ts
+++ src/core/production/production.service.ts
@@ -1,19 +1,123 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { OrderService } from './order.service';
-// import { describe, it, expect, beforeEach } from '@jest/globals';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateProductionDto } from './dto/create-production.dto';
+//import { CreateProductionItemsDto } from './dto/create-production.dto';
+import { UpdateProductionDto } from './dto/update-production.dto';
+import { ProductionRepository } from './production.repository';
+import { production_orders } from '@prisma/client';
+import { format } from 'date-fns';
 
-// describe('OrderService', () => {
-//   let service: OrderService;
+@Injectable()
+export class ProductionService {
+  constructor(private readonly productionRepository: ProductionRepository) {}
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [OrderService]
-//     }).compile();
+  async create(createProductionDto: CreateProductionDto) {
+    const existingOrder = await this.matchProductionByData(
+      createProductionDto.number
+      // createProductionDto.name,
+      // createProductionDto.price,
+      // createProductionDto.stock,
+      // createProductionDto.items
+    );
 
-//     service = module.get<OrderService>(OrderService);
-//   });
+    if (existingOrder.length > 0) {
+      throw new Error(
+        `Product already exists: ${JSON.stringify(existingOrder[0])}`
+      );
+    }
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+    const createdOrder =
+      await this.productionRepository.create(createProductionDto);
+
+    return this.formatProductionDate(createdOrder);
+  }
+  async matchProductionByData(number: number) {
+    //, name:string, price:number, stock: number, items: CreateProductionItemsDto[]) {
+    const matchedOrder = await this.productionRepository.matchProductionByData(
+      number
+      // name,
+      // price,
+      // stock,
+      // items
+    );
+
+    return matchedOrder.map(production =>
+      this.formatProductionDate(production)
+    );
+  }
+
+  async findAll(orderBy: string): Promise<
+    (Omit<production_orders, 'created_at' | 'updated_at'> & {
+      created_at: string;
+      updated_at: string;
+    })[]
+  > {
+    const findedProduction = await this.productionRepository.findAll(orderBy);
+    return findedProduction.map(production =>
+      this.formatProductionDate(production)
+    );
+  }
+
+  async findOne(id: number): Promise<
+    Omit<production_orders, 'created_at' | 'updated_at'> & {
+      created_at: string;
+      updated_at: string;
+    }
+  > {
+    const order = await this.productionRepository.findById(id);
+
+    if (!order) {
+      throw new NotFoundException(`Production with ID ${id} not found`);
+    }
+
+    return this.formatProductionDate(order);
+  }
+
+  async update(id: number, _updateProductionDto: UpdateProductionDto) {
+    const order = await this.isValid(id);
+
+    if (!order) {
+      throw new NotFoundException(`Product with ID ${id} not found`);
+    }
+    const updatedProductionDto = {
+      ..._updateProductionDto,
+      updated_at: new Date()
+    };
+
+    const updatedOrder = await this.productionRepository.update(
+      id,
+      updatedProductionDto
+    );
+
+    return this.formatProductionDate(updatedOrder);
+  }
+
+  async remove(id: number) {
+    await this.isValid(id);
+    await this.productionRepository.delete(id);
+  }
+
+  private formatProductionDate(production: production_orders): Omit<
+    production_orders,
+    'created_at' | 'updated_at'
+  > & {
+    created_at: string;
+    updated_at: string;
+  } {
+    return {
+      ...production,
+      created_at: format(
+        new Date(production.created_at),
+        'dd/MM/yyyy HH:mm:ss'
+      ),
+      updated_at: format(new Date(production.updated_at), 'dd/MM/yyyy HH:mm:ss')
+    };
+  }
+  async isValid(id: number) {
+    const order = await this.productionRepository.findById(id);
+
+    if (!order) {
+      throw new NotFoundException(`Product with ID ${id} not found`);
+    }
+    return order;
+  }
+}
