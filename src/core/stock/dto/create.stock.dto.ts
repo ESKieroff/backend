3fd22e5436b2d3be02b6e9b932b071dc85aa8945
ESKieroff <rodrigo.rosa81@edu.pucrs.br>
2024-10-17 00:16:24 -0300

    docs: dto for swagger docs

--- src/core/stock/dto/create.stock.dto.ts
+++ src/core/stock/dto/create.stock.dto.ts
@@ -1,140 +1,109 @@
-//import { ApiProperty } from '@nestjs/swagger';
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
 import { Stock_Moviment } from 'src/core/common/enums';
+import { object } from 'zod';
 
 export class CreateStockDto {
+  @ApiProperty({
+    description: 'Document date',
+    example: '2021-05-01',
+    required: true
+  })
   readonly document_date: Date;
+
+  @ApiHideProperty()
   readonly document_number: string;
+
+  @ApiProperty({
+    description: 'Stock moviment',
+    enum: ['INPUT', 'OUTPUT', 'RESERVED'],
+    required: true
+  })
   readonly stock_moviment: Stock_Moviment;
+
+  @ApiHideProperty()
   readonly is_balance?: boolean;
+
+  @ApiProperty({
+    description: 'Stock items',
+    type: object,
+    required: true
+  })
   readonly stock_items: CreateStockItemsDto[];
 }
 
 export class CreateStockItemsDto {
+  @ApiHideProperty()
   readonly stock_id: number;
+
+  @ApiHideProperty()
   readonly sequence: number;
+
+  @ApiProperty({
+    description: 'Product id',
+    example: '1',
+    required: true
+  })
   readonly product_id: number;
+
+  @ApiHideProperty()
   readonly lote: string;
+
+  @ApiHideProperty()
   readonly expiration: Date;
+
+  @ApiProperty({
+    description: 'Quantity',
+    example: '1',
+    required: true
+  })
   readonly quantity: number;
+
+  @ApiProperty({
+    description: 'Unit price',
+    example: '1.00',
+    required: false
+  })
   readonly unit_price?: number;
+
+  @ApiProperty({
+    description: 'Total price',
+    example: '1.00',
+    required: false
+  })
   readonly total_price?: number;
+
+  @ApiProperty({
+    description: 'Stock location id',
+    example: '1',
+    required: false
+  })
   readonly stock_location_id?: number;
+
+  @ApiProperty({
+    description: 'Supplier',
+    example: '1',
+    required: false
+  })
   readonly supplier?: number;
+
+  @ApiProperty({
+    description: 'Costumer',
+    example: '1',
+    required: false
+  })
   readonly costumer?: number;
+
+  @ApiProperty({
+    description: 'Observation',
+    example: 'Observação do produto',
+    required: false
+  })
   readonly observation?: string;
+
+  @ApiProperty({
+    description: 'Image link',
+    example: 'http://example.com/image.jpg',
+    required: false
+  })
   readonly image_link?: string;
 }
-
-// export class CreateStockDto {
-//   @ApiProperty({
-//     description: 'Document date',
-//     example: '2021-05-01'
-//   })
-//   readonly document_date: Date;
-
-//   @ApiProperty({
-//     description: 'Document number',
-//     example: '1'
-//   })
-//   readonly document_number: string;
-
-//   @ApiProperty({
-//     description: 'Stock moviment',
-//     example: ['INPUT', 'OUTPUT', 'TRANSIT']
-//   })
-//   readonly stock_moviment: Stock_Moviment;
-
-//   @ApiProperty({
-//     description: 'Flag for create balance and consolidate counts',
-//     default: 'false'
-//   })
-//   readonly is_balance: boolean;
-
-//   readonly stockitems: CreateStockItemsDto[];
-// }
-
-// export class CreateStockItemsDto {
-//   @ApiProperty({
-//     description: 'Stock id',
-//     example: '1'
-//   })
-//   readonly stock_id: number;
-
-//   @ApiProperty({
-//     description: 'Sequence',
-//     example: '1'
-//   })
-//   readonly sequence: number;
-
-//   @ApiProperty({
-//     description: 'Product id',
-//     example: '1'
-//   })
-//   readonly product_id: number;
-
-//   @ApiProperty({
-//     description: 'Quantity',
-//     example: '1'
-//   })
-//   readonly quantity: number;
-
-//   @ApiProperty({
-//     description: 'Unit price',
-//     example: '1.00'
-//   })
-//   readonly unit_price: number;
-
-//   @ApiProperty({
-//     description: 'Total price',
-//     example: '1.00'
-//   })
-//   readonly total_price: number;
-
-//   @ApiProperty({
-//     description: 'Lote',
-//     example: '1'
-//   })
-//   readonly lote: string;
-
-//   @ApiProperty({
-//     description: 'Expiration',
-//     example: '2021-05-01'
-//   })
-//   readonly expiration: Date;
-
-//   @ApiProperty({
-//     description: 'Supplier',
-//     example: '1'
-//   })
-//   readonly supplier: number;
-
-//   @ApiProperty({
-//     description: 'Costumer',
-//     example: '1'
-//   })
-//   readonly costumer: number;
-
-//   @ApiProperty({
-//     description: 'Stock location id',
-//     example: '1'
-//   })
-//   readonly stock_location_id: number;
-
-//   @ApiProperty({
-//     description: 'Observation',
-//     example: '1'
-//   })
-//   readonly observation: string;
-
-//   @ApiProperty({
-//     description: 'Image link',
-//     example: 'http://example.com/image.jpg'
-//   })
-//   readonly image_link: string;
-
-//   // @ApiProperty({
-//   //   description: 'Photo',
-//   //   example: 'Buffer[]'
-//   // })
-//   // readonly photo: Buffer[];
-// }
--- src/core/stock/dto/update.stock.dto.ts
+++ src/core/stock/dto/update.stock.dto.ts
@@ -1,25 +1,124 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateStockItemsDto } from './create.stock.dto';
 import { Stock_Moviment } from 'src/core/common/enums';
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
+
 export class UpdateStockDto {
+  @ApiHideProperty()
   updated_at?: Date;
+  @ApiHideProperty()
   updated_by?: number;
+
+  @ApiProperty({
+    description: 'Document date',
+    example: '2021-05-01',
+    required: false
+  })
   readonly document_date?: Date;
+  @ApiProperty({
+    description: 'Document number',
+    example: '0001',
+    required: false
+  })
   readonly document_number?: string;
+
+  @ApiProperty({
+    description: 'Stock moviment',
+    enum: ['INPUT', 'OUTPUT', 'RESERVED'],
+    required: false
+  })
   readonly stock_moviment?: Stock_Moviment;
+  @ApiHideProperty()
   readonly is_balance?: boolean;
+
+  @ApiProperty({
+    description: 'Stock items',
+    type: 'object',
+    required: true
+  })
   stock_items?: CreateStockItemsDto[];
 }
 export class UpdateStockItemsDto extends PartialType(CreateStockItemsDto) {
+  @ApiProperty({
+    description: 'Stock id',
+    example: '1',
+    required: false
+  })
   readonly id?: number;
+  @ApiProperty({
+    description: 'Product id',
+    example: '1',
+    required: false
+  })
   readonly sequence?: number;
-  readonly quantity: number;
-  readonly unit_price: number;
-  readonly total_price: number;
-  readonly lote: string;
-  readonly expiration: Date;
-  readonly supplier: number;
-  readonly costumer: number;
-  readonly stock_location_id: number;
-  readonly observation: string;
+
+  @ApiProperty({
+    description: 'Product id',
+    example: '1',
+    required: false
+  })
+  readonly quantity?: number;
+
+  @ApiProperty({
+    description: 'Unit price',
+    example: '1.00',
+    required: false
+  })
+  readonly unit_price?: number;
+
+  @ApiProperty({
+    description: 'Total price',
+    example: '1.00',
+    required: false
+  })
+  readonly total_price?: number;
+
+  @ApiProperty({
+    description: 'Lote',
+    example: '0001',
+    required: true
+  })
+  readonly lote?: string;
+
+  @ApiProperty({
+    description: 'Expiration date',
+    example: '2021-05-01',
+    required: false
+  })
+  readonly expiration?: Date;
+
+  @ApiProperty({
+    description: 'Product id',
+    example: '1',
+    required: false
+  })
+  readonly supplier?: number;
+
+  @ApiProperty({
+    description: 'Costumer id',
+    example: '1',
+    required: false
+  })
+  readonly costumer?: number;
+
+  @ApiProperty({
+    description: 'Stock location id',
+    example: '1',
+    required: false
+  })
+  readonly stock_location_id?: number;
+
+  @ApiProperty({
+    description: 'Observation',
+    example: 'This is a observation',
+    required: false
+  })
+  readonly observation?: string;
+
+  @ApiProperty({
+    description: 'Image link',
+    example: 'http://example.com/image.jpg',
+    required: false
+  })
+  readonly image_link?: string;
 }
