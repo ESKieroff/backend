7c52fb00671890f9a01425dcaa4e09ad5a35d3a6
ESKieroff <rodrigo.rosa81@edu.pucrs.br>
2024-10-26 01:17:11 -0300

    feat: module orchestrator

--- src/core/orchestrator/dto/response.orchestrator.dto.ts
+++ src/core/orchestrator/dto/response.orchestrator.dto.ts
@@ -1,7 +1,1 @@
-export class ResponseOrchestratorDto {
-  id: number;
-  active: boolean;
-  description: string;
-  created_at: string;
-  updated_at: string;
-}
+export class ResponseOrchestratorDto {}
--- src/core/orchestrator/dto/update.orchestrator.dto.ts
+++ src/core/orchestrator/dto/update.orchestrator.dto.ts
@@ -1,16 +1,1 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class UpdateOrchestratorDto {
-  @ApiProperty({
-    description: 'Category description',
-    minLength: 3,
-    example: 'Category'
-  })
-  readonly description: string;
-
-  @ApiProperty({
-    description: 'Category status',
-    example: true
-  })
-  readonly active: boolean;
-}
+export class UpdateOrchestratorDto {}
