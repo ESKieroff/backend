000f2dacc87d94727a294a4ca345a4dbc2d7e127
ESKieroff <rodrigo.rosa81@edu.pucrs.br>
2024-11-01 17:10:58 -0300

    feat: endpoint renamed

--- src/core/production-steps/production-steps.controller.ts
+++ src/core/production-steps/production-steps.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { ProductionStepsService } from './production-steps.service';
 
-@Controller('production-steps')
+@Controller('steps')
 export class ProductionStepsController {
   constructor(private readonly categoriesService: ProductionStepsService) {}
 }
