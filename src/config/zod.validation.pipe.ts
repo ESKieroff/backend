2c091317844e5df043bb1268c119c4e7ba7b7e88
ESKieroff <rodrigo.rosa81@edu.pucrs.br>
2024-11-06 19:32:50 -0300

    feat: console

--- src/config/zod.validation.pipe.ts
+++ src/config/zod.validation.pipe.ts
@@ -11,13 +11,13 @@ import { filterExtraFields } from 'src/core/common/filter.fields';
 
 @Injectable()
 export class ZodValidationPipe implements PipeTransform {
-  transform(value: any, _metadata: ArgumentMetadata) {
-    //console.log('--- ZodValidationPipe Debug Start ---');
-    //console.log('Valor recebido para validação:', value);
-    //console.log('Metadata:', metadata);
+  transform(value: any, metadata: ArgumentMetadata) {
+    console.log('--- ZodValidationPipe Debug Start ---');
+    console.log('Valor recebido para validação:', value);
+    console.log('Metadata:', metadata);
 
     if (value === undefined || value === null) {
-      //console.log('Valor indefinido ou nulo. Ignorando validação.');
+      console.log('Valor indefinido ou nulo. Ignorando validação.');
       return value;
     }
 
@@ -29,14 +29,14 @@ export class ZodValidationPipe implements PipeTransform {
     let path = value.__path;
 
     if (!method || !path) {
-      //console.log('Método ou caminho ausente. Ignorando validação.');
+      console.log('Método ou caminho ausente. Ignorando validação.');
       delete value.__method;
       delete value.__path;
       return value;
     }
 
     if (method === 'GET') {
-      //console.log('Método GET detectado. Ignorando validação.');
+      console.log('Método GET detectado. Ignorando validação.');
       delete value.__method;
       delete value.__path;
       return value;
@@ -47,22 +47,22 @@ export class ZodValidationPipe implements PipeTransform {
     }
 
     const routeKey = `${method}:${path}`;
-    //console.log('routeKey gerado:', routeKey);
+    console.log('routeKey gerado:', routeKey);
 
     const schema = ZodSchemasMap[routeKey] as ZodSchema<any>;
     if (!schema) {
-      //console.log(
-      //   `Schema não encontrado para a rota ${routeKey}. Retornando valor sem validação.`
-      // );
+      console.log(
+        `Schema não encontrado para a rota ${routeKey}. Retornando valor sem validação.`
+      );
       delete value.__method;
       delete value.__path;
       return value;
     }
 
-    //console.log(`Schema encontrado para ${routeKey}. Iniciando validação.`);
+    console.log(`Schema encontrado para ${routeKey}. Iniciando validação.`);
 
     const filteredData = filterExtraFields(value, schema as ZodObject<any>);
-    //console.log('Dados filtrados:', filteredData);
+    console.log('Dados filtrados:', filteredData);
 
     const validationResult = schema.safeParse(filteredData);
     if (!validationResult.success) {
@@ -71,15 +71,15 @@ export class ZodValidationPipe implements PipeTransform {
         message: err.message
       }));
 
-      //console.log('Erros de validação encontrados:', errors);
+      console.log('Erros de validação encontrados:', errors);
       throw new BadRequestException({
         message: 'Erro de validação',
         data: errors
       });
     }
 
-    // console.log('Validação bem-sucedida. Retornando valor validado.');
-    // console.log('--- ZodValidationPipe Debug End ---');
+    console.log('Validação bem-sucedida. Retornando valor validado.');
+    console.log('--- ZodValidationPipe Debug End ---');
 
     delete value.__method;
     delete value.__path;
